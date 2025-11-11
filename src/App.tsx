b38451dd091d35b3dc483ad3947201a46bd48732
codexwork3 <49438899-codexwork3@users.noreply.replit.com>
2025-11-11 08:50:40 +0000

    FIX: Remove all references to deleted ProtectedLayout component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import { Toaster } from "sonner"
-import { Login } from "./components/auth/Login"
-import { Signup } from "./components/auth/Signup"
-import { OnboardingCarousel } from "./components/OnboardingCarousel"
-import { BiometricLogin } from "./components/auth/BiometricLogin"
-import { Dashboard } from "./components/dashboard/Dashboard"
-import { Deposit } from "./components/wallet/Deposit"
-import { Withdraw } from "./components/wallet/Withdraw"
-import { AdminPanel } from "./components/admin/AdminPanel"
-import { UserList } from "./components/admin/UserList"
-import { TransactionHistory } from "./components/wallet/TransactionHistory"
-import ErrorBoundary from "./components/ErrorBoundary"
-import { ProtectedLayout } from "./components/ProtectedLayout"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "sonner";
+import { Login } from "./components/auth/Login";
+import { Signup } from "./components/auth/Signup";
+import { OnboardingCarousel } from "./components/OnboardingCarousel";
+import { BiometricLogin } from "./components/auth/BiometricLogin";
+import { Dashboard } from "./components/dashboard/Dashboard";
+import { Deposit } from "./components/wallet/Deposit";
+import { Withdraw } from "./components/wallet/Withdraw";
+import { AdminPanel } from "./components/admin/AdminPanel";
+import { UserList } from "./components/admin/UserList";
+import { TransactionHistory } from "./components/wallet/TransactionHistory";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function LoginWithBiometric() {
   return (
@@ -21,7 +20,7 @@ function LoginWithBiometric() {
         <BiometricLogin />
       </div>
     </div>
-  )
+  );
 }
 
 function App() {
@@ -34,20 +33,18 @@ function App() {
           <Route path="/login" element={<LoginWithBiometric />} />
           <Route path="/signup" element={<Signup />} />
 
-          <Route element={<ProtectedLayout />}>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/deposit" element={<Deposit />} />
-            <Route path="/withdraw" element={<Withdraw />} />
-            <Route path="/history" element={<TransactionHistory />} />
-            <Route path="/admin" element={<AdminPanel />} />
-            <Route path="/admin/users" element={<UserList />} />
-          </Route>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/deposit" element={<Deposit />} />
+          <Route path="/withdraw" element={<Withdraw />} />
+          <Route path="/history" element={<TransactionHistory />} />
+          <Route path="/admin" element={<AdminPanel />} />
+          <Route path="/admin/users" element={<UserList />} />
 
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
     </ErrorBoundary>
-  )
+  );
 }
 
-export default App
+export default App;
